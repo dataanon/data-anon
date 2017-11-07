3ecf1341d3acf14f99a744f136126cacb34559a0
sarthak <msarthak@thoughtworks.com>
2017-11-07 16:56:20 +0530

    Sarthak: Changed version from RELEASE to specific version and integrated jacoco

--- src/main/kotlin/com/github/dataanon/Field.kt
+++ src/main/kotlin/com/github/dataanon/Field.kt
@@ -1,3 +1,3 @@
 package com.github.dataanon
 
-data class Field<T: Any>(val name: String, val oldValue: T, var newValue: T = oldValue)
+class Field<T: Any>(val name: String, val oldValue: T, var newValue: T = oldValue)
--- src/main/kotlin/com/github/dataanon/Record.kt
+++ src/main/kotlin/com/github/dataanon/Record.kt
@@ -1,6 +1,6 @@
 package com.github.dataanon
 
-data class Record(private val fields: List<Field<Any>>, val rowNum: Int) {
+class Record(private val fields: List<Field<Any>>, val rowNum: Int) {
 
     fun find(name: String): Field<Any> = fields.first {name.equals(it.name, true)}
 }
