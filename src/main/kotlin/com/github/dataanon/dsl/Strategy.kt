850d8b28ba68cdecd07b85e8b4f668c8ad43c72b
sarthak <msarthak@thoughtworks.com>
2017-11-08 23:05:36 +0530

    Sarthak: Added Flux support for all table collection in Strategy

--- src/main/kotlin/com/github/dataanon/dsl/Strategy.kt
+++ src/main/kotlin/com/github/dataanon/dsl/Strategy.kt
@@ -11,15 +11,16 @@ abstract class Strategy {
     protected val tables = mutableListOf<Table>()
 
     fun execute(limit: Long = -1, progressBarEnabled: Boolean = true) {
-        tables.forEach { table ->
-            val reader = TableReader(sourceDbConfig(), table, limit)
-            val progressBar = ProgressBarGenerator(progressBarEnabled, table.name, { reader.totalNoOfRecords() })
-            val writer = TableWriter(destDbConfig(), table, progressBar)
+        Flux.fromIterable(tables)
+                .subscribe { table -> executeOnTable(table, limit, progressBarEnabled) }
+    }
 
-            Flux.fromIterable(Iterable { reader })
-                    .map { table.execute(it) }
-                    .subscribe(writer)
-        }
+    private fun executeOnTable(table: Table, limit: Long, progressBarEnabled: Boolean) {
+        val reader      = TableReader(sourceDbConfig(), table, limit)
+        val progressBar = ProgressBarGenerator(progressBarEnabled, table.name, { reader.totalNoOfRecords() })
+        val writer      = TableWriter(destDbConfig(), table, progressBar)
+
+        Flux.fromIterable(Iterable { reader }).map { table.execute(it) }.subscribe(writer)
     }
 
     abstract protected fun sourceDbConfig(): DbConfig
