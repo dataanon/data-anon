2bea59f700aff58d9ff8799df9468378fd7ba106
sarthak <msarthak@thoughtworks.com>
2018-03-13 18:58:25 +0530

    Sarthak: Refactored PickFromDatabase

--- src/main/kotlin/com/github/dataanon/strategy/list/PickFromDatabase.kt
+++ src/main/kotlin/com/github/dataanon/strategy/list/PickFromDatabase.kt
@@ -6,17 +6,25 @@ import com.github.dataanon.model.Record
 import com.github.dataanon.strategy.AnonymizationStrategy
 import com.github.dataanon.utils.RandomSampling
 
-class PickFromDatabase<T: Any>(dbConfig: DbConfig, selectQuery: String) : AnonymizationStrategy<T>, RandomSampling {
-    val values = mutableListOf<T>()
+class PickFromDatabase<T : Any>(dbConfig: DbConfig, selectQuery: String) : AnonymizationStrategy<T>, RandomSampling {
 
-    init {
-        val conn = dbConfig.connection()
-        val stmt = conn.createStatement()
+    private val values = initializeValues(dbConfig, selectQuery)
+
+    private fun initializeValues(dbConfig: DbConfig, selectQuery: String): List<T> {
+        val conn   = dbConfig.connection()
+        val stmt   = conn.createStatement()
         val result = stmt.executeQuery(selectQuery)
-        while (result.next()) values += result.getObject(1) as T
+
+        val list = generateSequence {
+            if (result.next()) result.getObject(1) as T else null
+        }.toList()
+
         stmt.close()
         conn.close()
-        require(values.isNotEmpty(), {"values cannot be empty while using PickFromDatabase"})
+
+        require(list.isNotEmpty(), { "values cannot be empty while using PickFromDatabase" })
+        return list
     }
+
     override fun anonymize(field: Field<T>, record: Record): T = sample(values)
 }
